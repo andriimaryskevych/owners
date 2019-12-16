c2d04b513b2b1dad813a8e00664def2ea3a12d44
Andrii Maryskevych <andrii.maryskevych@gmail.com>
2019-12-16 20:05:48 +0200

    Fix image path

--- api/services/ServiceService.js
+++ api/services/ServiceService.js
@@ -6,10 +6,9 @@ const FileService = require('./FileService');
 
 class ServiceService {
     async create ({ tag, userId, title, description, phone, email }, image) {
-        const photoPath = `${userId}/service/image/${uuid.v4()}/${image.originalname}`;
-        await FileService.saveFile(photoPath, image.buffer);
+        const photoUrl = `${userId}/service/image/${uuid.v4()}/${image.originalname}`;
+        await FileService.saveFile(photoUrl, image.buffer);
 
-        const photoUrl = `https://sometext.xyz/${photoPath}`;
         const service = new Service({ userId, tag, photoUrl, title, description, phone, email });
         await service.save();
 
